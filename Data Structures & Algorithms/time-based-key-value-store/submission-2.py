afab4471c3bff42c99f221794b9d96af47c189b1
Sherzod Kariev <43387955+scoobyd0027@users.noreply.github.com>
2026-09-22 10:00:14 -0700

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from bisect import bisect_left
+
+class TimeMap:
+
+    def __init__(self):
+        self.map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.map[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if not self.map[key]: return ""
+        i = bisect_left(self.map[key], (timestamp,))
+        # print(timestamp, self.map[key])
+        return self.map[key][i][1] if i < len(self.map[key]) and self.map[key][i][0] == timestamp else self.map[key][i - 1][1] if i != 0 else ""
+
+'''
+(10, "one"), (20, "two"), (30, "three")
+
+(1, "happy"), (2, "alice"), (3, "sad")
+'''
